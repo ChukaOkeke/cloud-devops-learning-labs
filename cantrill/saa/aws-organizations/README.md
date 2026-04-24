817f2ce621e4cdf641fe383157ff09e874b46472
ChukaOkeke <chukaookeke@gmail.com>
2026-04-24 12:11:37 +0100

    create an aws organization

---
+++ cantrill/saa/aws-organizations/README.md
@@ -0,0 +1,5 @@
+This project:
+- Creates an AWS Organization
+- Invites an existing AWS account 
+- Creates an admin role within the invited account for role-switching
+- Creates a new account within the Organization 
---
+++ cantrill/saa/aws-organizations/main.tf
@@ -0,0 +1,72 @@
+# Configure the Terraform AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Providers for Master and PROD accounts
+# Master account provider
+provider "aws" {
+  region = "us-east-1"
+  profile = "iamadmin-general" # Specify the AWS CLI profile for the general account (optional)
+  # Access keys can be set in the environment variables or through the AWS CLI configuration
+}
+
+# PROD account provider (Points to PROD)
+provider "aws" {
+  alias  = "prod_account"
+  region = "us-east-1"
+  profile = "iamadmin-prod" # Specify the AWS CLI profile for the PROD account (optional)
+#   assume_role {
+#     # This is the role you manually use or create to "get into" PROD
+#     role_arn = "arn:aws:iam::PROD_ACCOUNT_ID:role/OrganizationAccountAccessRole"
+#   }
+}
+
+# Create an AWS Organization with ALL features enabled and allow access to IAM and CloudTrail services for the organization
+resource "aws_organizations_organization" "org" {
+  feature_set = "ALL"
+  aws_service_access_principals = [
+    "iam.amazonaws.com",
+    "cloudtrail.amazonaws.com"
+  ]
+}
+
+# Track the PROD account manually invited into the organization in the Terraform state (run terraform import aws_organizations_account.prod 697790871179 after manually inviting the account)
+resource "aws_organizations_account" "prod" {
+  name  = "Production"
+  email = "liamrosie@hotmail.com"
+}
+
+# Create Role in PROD account to allow Master account to assume it and role-switch
+resource "aws_iam_role" "cross_account_role" {
+  provider = aws.prod_account  # Specify the provider for the PROD account
+  name     = "OrganizationAccountAccessRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Action    = "sts:AssumeRole"
+      Principal = { AWS = "arn:aws:iam::045511192978:root" } # Replace with the actual AWS Account ID of the Master account
+    }]
+  })
+}
+
+# Attach the AdministratorAccess policy to the cross-account role in the PROD account
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  provider = aws.prod_account  # Specify the provider for the PROD account
+  role       = aws_iam_role.cross_account_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Create a new Development member account within the Organization
+resource "aws_organizations_account" "dev" {
+  name      = "Development"
+  email     = "chukybombo@yahoo.com"
+  role_name = "OrganizationAccountAccessRole" # Created automatically by AWS
+}
